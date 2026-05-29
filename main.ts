64637312016b418057c54ec5ebf4971754c66921
oiAmigosSTEAM <a2551@escolaglobal.org>
2026-05-29 16:35:32 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+let Grego = sprites.create(assets.image`Grego frente`, SpriteKind.Player)
+let Troiano = sprites.create(assets.image`Troiano frente`, SpriteKind.Player)
+let Ulisses = sprites.create(assets.image`Ulisses frente`, SpriteKind.Player)
+let Princesa = sprites.create(assets.image`Princesa frente`, SpriteKind.Player)
+let Canhão = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
